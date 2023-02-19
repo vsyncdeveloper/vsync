4b4789d00ce79919600932fcd9e6eb5d92a5b1fa
Zoë <116910306+vsyncdeveloper@users.noreply.github.com>
2023-02-19 13:34:05 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ if game.PlaceId == 9872472334 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vsyncdeveloper/vsync/main/Evade.lua"))()
 else
     if game.PlaceId == 6902912928 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/vsyncdeveloper/vsync/main/Get%20to%20the%20top.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vsyncdeveloper/vsync/main/Get-to-the-top.lua"))()
     else
         if game.Placeid == 5942932659 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/vsyncdeveloper/vsync/main/Wormface!.lua"))()
